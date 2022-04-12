f77a93507313b3e7ba87828ba0b5c810c5bbbfff
Hallef Vieira <hallef.vieira@hotmail.com>
2022-04-12 20:39:56 -0300

    feat: Add swagger docs to app

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
 import { app } from ".";
 
-app.listen(3333, () => console.log("Server is running!"));
+app.listen(3333, () =>
+  console.log({
+    server_status: "Server is running! 🤯️🚀️",
+    doc_adress: "http://localhost:3333/api-docs/#/Users",
+  })
+);
